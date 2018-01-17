3d1dfae9043570edab272c7d3b8a2986b9c0b260
Reza Asad <reza@Rezas-MacBook-Pro.local>
2018-01-16 17:04:50 -0800

    batch normalization added to the 3 layer cnn

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -18,8 +18,8 @@ class ThreeLayerConvNet(object):
     """
 
     def __init__(self, dropout=0, input_dim=(3, 32, 32), num_filters=32, filter_size=7,
-                 hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0,
-                 dtype=np.float32):
+                 hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0, 
+                 use_batch_norm=False, dtype=np.float32):
         """
         Initialize a new network.
 
@@ -34,9 +34,11 @@ class ThreeLayerConvNet(object):
         - reg: Scalar giving L2 regularization strength
         - dtype: numpy datatype to use for computation.
         """
-        self.usedropout = dropout > 0
+        self.use_dropout = dropout > 0
+        self.use_batch_norm = use_batch_norm
         self.params = {}
         self.reg = reg
+        self.num_layers = 3
         self.dtype = dtype
         self.pool_height = 2
         self.pool_width = 2
@@ -56,17 +58,27 @@ class ThreeLayerConvNet(object):
         C, H, W = input_dim
         self.params['W1'] = np.random.randn(num_filters, C, filter_size, filter_size) * weight_scale
         self.params['b1'] = np.zeros(num_filters)
-        H_pool = (H - self.pool_height) / 2 + 1
-        W_pool = (W - self.pool_width) / 2 + 1
-        self.params['W2'] = np.random.randn(np.prod((num_filters, H_pool, W_pool)), hidden_dim) * weight_scale
+        #H_pool = (H - self.pool_height) / 2 + 1
+        #W_pool = (W - self.pool_width) / 2 + 1
+        self.params['W2'] = np.random.randn(np.prod((num_filters, H, W)), hidden_dim) * weight_scale
         self.params['b2'] = np.zeros(hidden_dim)
         self.params['W3'] = np.random.randn(hidden_dim, num_classes) * weight_scale
         self.params['b3'] = np.zeros(num_classes)
 
+        # Create parameters for batch normalization if necessary
+        self.params['gamma1'] = np.ones(num_filters) 
+        self.params['beta1'] = np.zeros(num_filters)
+        self.params['gamma2'] = np.ones(hidden_dim)
+        self.params['beta2'] = np.zeros(hidden_dim)
+
         # Set dropout parameters if necessary
         self.dropout_param={}
-        if self.usedropout:
+        if self.use_dropout:
             self.dropout_param ={'mode':'train', 'p':dropout}
+
+        self.bn_params = []
+        if self.use_batch_norm:
+            self.bn_params = [{'mode': 'train'} for i in range(self.num_layers - 1)]
 
         ############################################################################
         #                             END OF YOUR CODE                             #
@@ -81,9 +93,18 @@ class ThreeLayerConvNet(object):
 
         Input / output: Same API as TwoLayerNet in fc_net.py.
         """
+        X = X.astype(self.dtype)
+        mode = 'test' if y is None else 'train'
+
         W1, b1 = self.params['W1'], self.params['b1']
         W2, b2 = self.params['W2'], self.params['b2']
         W3, b3 = self.params['W3'], self.params['b3']
+
+        if self.use_batch_norm:
+            gamma1 = self.params['gamma1']
+            beta1 = self.params['beta1']
+            gamma2 = self.params['gamma2']
+            beta2 = self.params['beta2']
 
         # pass conv_param to the forward pass for the convolutional layer
         filter_size = W1.shape[2]
@@ -92,14 +113,37 @@ class ThreeLayerConvNet(object):
         pool_param = {'pool_height': self.pool_height, 'pool_width': self.pool_width, 
                       'stride': self.pool_stride}
 
+        # Set train/test mode for batchnorm params and dropout param since they
+        # behave differently during training and testing.
+        if self.use_dropout:
+            self.dropout_param['mode'] = mode
+        if self.use_batch_norm:
+            for bn_param in self.bn_params:
+                bn_param['mode'] = mode
+
         ############################################################################
         # TODO: Implement the forward pass for the three-layer convolutional net,  #
         # computing the class scores for X and storing them in the scores          #
         # variable.                                                                #
         ############################################################################
-        first_layer_scores, first_layer_cache = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
-        second_layer_scores, second_layer_cache = affine_relu_forward(first_layer_scores, W2, b2, dropout=self.usedropout,
-                                                                      dropout_param=self.dropout_param)
+        if self.use_batch_norm:
+            first_layer_scores, first_layer_cache = conv_bn_relu_forward(X, W1, b1,
+                                                                         gamma1, beta1, 
+                                                                         conv_param, 
+                                                                         self.bn_params[0])
+            second_layer_scores, second_layer_cache = affine_bn_relu_forward(first_layer_scores,
+                                                                             W2, b2, gamma2, beta2, 
+                                                                             self.bn_params[1], 
+                                                                             dropout=self.use_dropout, 
+                                                                             dropout_param=self.dropout_param)
+        else:
+            first_layer_scores, first_layer_cache = conv_relu_pool_forward(X, W1, b1, conv_param,
+                                                                           pool_param)
+            second_layer_scores, second_layer_cache = affine_relu_forward(first_layer_scores, 
+                                                                          W2, b2, 
+                                                                          dropout=self.use_dropout,
+                                                                          dropout_param=self.dropout_param)
+
         scores, output_cache = affine_forward(second_layer_scores, W3, b3)
         ############################################################################
         #                             END OF YOUR CODE                             #
@@ -124,15 +168,31 @@ class ThreeLayerConvNet(object):
         grads['W2'] = 2 * self.reg * np.sum(W2 * W2)
         grads['W3'] = 2 * self.reg * np.sum(W3 * W3)
 
-        dx, dw, db = affine_backward(dscores, output_cache)
-        grads['W3'] += dw
-        grads['b3'] = db
-        dx, dw, db = affine_relu_backward(dx, second_layer_cache, dropout=self.usedropout)
-        grads['W2'] += dw
-        grads['b2'] = db
-        _, dw, db = conv_relu_pool_backward(dx, first_layer_cache)
-        grads['W1'] += dw
-        grads['b1'] = db
+        if self.use_batch_norm:
+            dx, dw, db = affine_backward(dscores, output_cache)
+            grads['W3'] += dw
+            grads['b3'] = db
+            dx, dw, db, dgamma, dbeta = affine_bn_relu_backward(dx, second_layer_cache, dropout=self.use_dropout)
+            grads['W2'] += dw
+            grads['b2'] = db
+            grads['gamma2'] = dgamma
+            grads['beta2'] = dbeta
+            _, dw, db, dgamma, dbeta = conv_bn_relu_backward(dx, first_layer_cache)
+            grads['W1'] += dw
+            grads['b1'] = db
+            grads['gamma1'] = dgamma
+            grads['beta1'] = dbeta
+
+        else:
+            dx, dw, db = affine_backward(dscores, output_cache)
+            grads['W3'] += dw
+            grads['b3'] = db
+            dx, dw, db = affine_relu_backward(dx, second_layer_cache, dropout=self.use_dropout)
+            grads['W2'] += dw
+            grads['b2'] = db
+            _, dw, db = conv_relu_pool_backward(dx, first_layer_cache)
+            grads['W1'] += dw
+            grads['b1'] = db
 
         ############################################################################
         #                             END OF YOUR CODE                             #
